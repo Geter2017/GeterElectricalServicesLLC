6857c81e2092063931e347ba2e093049919f7289
Don <Donmiller150@yahoo.com>
2023-11-11 22:43:00 -0500

    Finally was able to get logo responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Headline from "./components/headline.component";
-import Introduction from "./components/Introduction/introduction.component";
 
 function App() {
 	return (
 		<div className="App">
 			<Headline />
-			<Introduction />
 		</div>
 	);
 }
--- src/components/headline.component.jsx
+++ src/components/headline.component.jsx
@@ -4,32 +4,34 @@ import "./headline.styles.css";
 
 export default function Headline() {
 	return (
-		<div className="cover">
-			<img
-				style={{ width: "100%", height: "100%", position: "relative" }}
-				src="HeaderPhoto.jpg"
-				alt="Head"
-			/>
-			<img className="thelogo" src={TheLogo} alt="description" />
-			<div className="button">
-				{/* Need to come back to button to add on click logic before deploying the website live */}
-				<button
-					className="button"
-					style={{
-						width: 200,
-						height: 60,
-						background: "#FEFFFE",
-						borderRadius: 20,
-						border: "10px #131552 solid",
+		<div className="container">
+			<div className="cover">
+				<img
+					style={{ width: "100%", height: "100%", position: "relative" }}
+					src="HeaderPhoto.jpg"
+					alt="Head"
+				/>
+				<img className="thelogo" src={TheLogo} alt="description" />
+				<div className="button">
+					{/* Need to come back to button to add on click logic before deploying the website live */}
+					<button
+						className="button"
+						style={{
+							width: 200,
+							height: 60,
+							background: "#FEFFFE",
+							borderRadius: 20,
+							border: "10px #131552 solid",
 
-						fontSize: 20,
-						fontFamily: "Istok Web",
-						fontWeight: "400",
-						textShadow: "3px 3px 5px #FF8D23 ",
-					}}
-				>
-					SCHEDULE NOW
-				</button>
+							fontSize: 20,
+							fontFamily: "Istok Web",
+							fontWeight: "400",
+							textShadow: "3px 3px 5px #FF8D23 ",
+						}}
+					>
+						SCHEDULE NOW
+					</button>
+				</div>
 			</div>
 		</div>
 	);
