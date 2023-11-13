1f5d76a11d4f220dd4c43623b8a1fe89780cd334
Don <Donmiller150@yahoo.com>
2023-11-13 16:22:53 -0500

    Was able to add background photos and text for website still need to create submit form and add logice to the schedule now button and form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Headline from "./components/headline.component";
 import Introduction from "./components/Introduction.components";
+import Invite from "./components/who-we-are.components";
 
 function App() {
 	return (
 		<div className="App">
 			<Headline />
 			<Introduction />
+			<Invite />
 		</div>
 	);
 }
---
+++ src/components/who-we-are.components.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./who-we-are.styles.css";
+
+export default function Invite() {
+	return (
+		<div className="body">
+			<div className="cover">
+				<img
+					style={{ width: "100%", height: "100%", position: "relative" }}
+					src="thebody.jpg"
+					alt="Head"
+				/>
+			</div>
+		</div>
+	);
+}
