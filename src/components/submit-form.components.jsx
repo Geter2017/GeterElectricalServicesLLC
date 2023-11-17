1759b1650e74a5aff36f6d9891e5561dca6afb2a
Don <Donmiller150@yahoo.com>
2023-11-17 02:02:22 -0500

    updated the contact form but there is issues. I need to resize to make all contents fit in the site

--- src/components/submit-form.components.jsx
+++ src/components/submit-form.components.jsx
@@ -53,16 +53,14 @@ const Contact = () => {
 
 	return (
 		<form className="form" onSubmit={handleSubmit}>
-			<h1>Schedule Appointment</h1>
-
-			<label className="fullname">Full Name(required)</label>
+			{/* <h1>Schedule Appointment</h1> */}
+			<label className="name">Full Name(required)</label>
 			<input
 				className="fullname"
 				placeholder="Full Name"
 				value={fullname}
 				onChange={(e) => setFullName(e.target.value)}
 			/>
-
 			<label className="email">Email(required)</label>
 			<input
 				className="email"
@@ -70,7 +68,6 @@ const Contact = () => {
 				value={email}
 				onChange={(e) => setEmail(e.target.value)}
 			/>
-
 			<label className="phonenumber">Phone Number(required)</label>
 			<input
 				className="phonenumber"
@@ -78,46 +75,38 @@ const Contact = () => {
 				value={phonenumber}
 				onChange={(e) => setPhoneNumber(e.target.value)}
 			/>
-
-			<label className="date">Date & Time(required)</label>
-			<input
-				className="date"
-				placeholder="Date Time"
-				value={datetime}
-				onChange={(e) => setDateTime(e.target.value)}
-			/>
-
-			<label className="city">City(required)</label>
-			<input
-				className="city"
-				placeholder="City"
-				value={city}
-				onChange={(e) => setCity(e.target.value)}
-			/>
-
-			<label className="state">State(required)</label>
-			<input
-				className="state"
-				placeholder="State"
-				value={state}
-				onChange={(e) => setState(e.target.value)}
-			/>
-
-			<label className="zip">ZipCode(required)</label>
-			<input
-				className="zip"
-				placeholder="Zip Code"
-				value={zipcode}
-				onChange={(e) => setZipCode(e.target.value)}
-			/>
-
+			{/* <label className="date">Date & Time(required)</label> */}
+			{/* <input */}
+			{/* className="date" */}
+			{/* placeholder="Date Time" */}
+			{/* value={datetime} */}
+			{/* onChange={(e) => setDateTime(e.target.value)} */}
+			{/* /> */}
+			{/* <label className="city">City(required)</label> */}
+			{/* <input */}
+			{/* className="city" placeholder="City" value={city} */}
+			{/* onChange={(e) => setCity(e.target.value)} */}
+			{/* /> */}
+			{/* <label className="state">State(required)</label> */}
+			{/* <input */}
+			{/* className="state" */}
+			{/* placeholder="State" */}
+			{/* value={state} */}
+			{/* onChange={(e) => setState(e.target.value)} */}
+			{/* /> */}
+			{/* <label className="zip">ZipCode(required)</label> */}
+			{/* <input */}
+			{/* className="zip" */}
+			{/* placeholder="Zip Code" */}
+			{/* value={zipcode} */}
+			{/* onChange={(e) => setZipCode(e.target.value)} */}
+			{/* /> */}
 			<label>Message</label>
 			<textarea
 				placeholder="Message"
 				value={message}
 				onChange={(e) => setMessage(e.target.value)}
 			></textarea>
-
 			<button
 				type="submit"
 				style={{ background: loader ? "#ccc" : " rgb(2, 2, 110)" }}
