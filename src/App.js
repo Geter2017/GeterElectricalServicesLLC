eb859363758322d519f31c3c6f13a347ece11aad
Don <Donmiller150@yahoo.com>
2023-11-21 04:55:25 -0500

    Complete website application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import ScheduleButton from "./components/schedule-button.component";
 import Invite from "./components/who-we-are.components";
-import Contact from "./components/submit-form.components";
 
 function App() {
 	return (
 		<div className="App">
 			<Invite />
-			<Contact />
-			<ScheduleButton />
 		</div>
 	);
 }
